e757257bc5829571e9e1928b035f74b170c97b4c
Andrew Niemantsverdriet <andrewniemants@gmail.com>
2013-03-27 11:23:49 -0600

    Inital Code Check-in

---
+++ js/util.js
@@ -0,0 +1,41 @@
+//// Edit the options.url to point to your URL
+//$.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+//	options.url = 'http://zinc.rocky.edu:8000' + options.url;
+//});
+
+$.ajaxSetup({
+	statusCode: {
+		401: function(){
+			// Show the login modal box
+			$('#login').modal('show');
+		},
+		403: function() {
+			// 403 -- Access denied (not used right now)
+			$("#access-error").show();
+		}
+	}
+});
+
+// Permission Denied Error
+$("#error-close").click(function() {
+	$("#access-error").hide();
+});
+
+// Login Function
+$('#loginForm').submit(function(e) {
+	e.preventDefault();
+
+	var login;
+	login = $.ajax({
+		type: "POST",
+		url: '/login',
+		headers: {"Accept": "application/json"},
+		dataType: 'json',
+		data: $("#loginForm").serialize()
+	})
+	.done(function(data) {
+		location.reload();
+		console.log("Logged In");
+	})
+	.fail(function() { alert("error logging in"); });
+});
