15705fc426516dea4af36b67fad832d1a84ba198
Andrew Niemantsverdriet <andrewniemants@gmail.com>
2013-03-28 15:16:57 -0600

    Seperate out the controllers from the main app

--- js/util.js
+++ js/util.js
@@ -15,22 +15,3 @@ $.ajaxSetup({
 $("#error-close").click(function() {
 	$("#access-error").hide();
 });
-
-// Login Function
-$('#loginForm').submit(function(e) {
-	e.preventDefault();
-
-	var login;
-	login = $.ajax({
-		type: "POST",
-		url: '/login',
-		headers: {"Accept": "application/json"},
-		dataType: 'json',
-		data: $("#loginForm").serialize()
-	})
-	.done(function(data) {
-		location.reload();
-		console.log("Logged In");
-	})
-	.fail(function() { alert("error logging in"); });
-});
